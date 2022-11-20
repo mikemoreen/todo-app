90062951549f006ac2f2900f1b8729a6e5fe5852
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2022-11-20 22:33:51 +0400

    ...

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,7 +1,7 @@
 const CardForm: React.FC = () =>{
     
     return (
-    <div className='bg-neutral-300 h-48 w-48 py-5 mr-4 mb-8'>{}</div>
+    <div className='bg-neutral-300 h-48 w-48 py-5 mr-4 mb-8'>{sdsadsad}</div>
     )
 }
 export default CardForm;
